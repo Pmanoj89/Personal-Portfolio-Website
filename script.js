99f9f3b07967cd421ce515312c6c94fe818e3d06
Manoj Pal <mpal29237@gmail.com>
2026-03-10 10:27:37 +0000

    save

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage(){
+alert("Thank you for visiting my portfolio!");
+}
+
+document.getElementById("contactForm").addEventListener("submit",function(e){
+e.preventDefault();
+alert("Message sent successfully!");
+});
